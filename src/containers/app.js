8fd1cf96ac31ec0e9b3abc7ffebb7a8f6842ebcd
Bkake <bangaly.kake@gmail.com>
2018-06-02 16:10:17 +0200

    Get movie list via api db video and update VideoList

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import SearchBar from '../components/search-bar'
+import React, {Component} from 'react';
+import SearchBar from '../components/search-bar';
 import VideoList from './video-list';
+import axios from 'axios';
 
-const App = () => {
-    return(
-        <div>
-            <SearchBar/>
-            <VideoList/>
-        </div>
-    ) 
+const API_END_POINT = "https://api.themoviedb.org/3/"
+const POPULAR_MOVIES_URL = "discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images"
+const API_KEY = "api_key=fb32c1b6b2f144f9404dd6757c23361d"
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {currentMovie:{}, movieList:{}};
+    }
+
+    componentWillMount() {
+      this.initMovies();
+    }
+
+    initMovies() {
+        axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
+            this.setState({currentMovie: response.data.results[0], movieList: response.data.results.slice(1,6)})  
+          }.bind(this));
+    }
+
+    render() {
+        const renderVideoList = () => {
+            if(this.state.movieList.length>=5) {
+                return <VideoList movieList={this.state.movieList}/> 
+            }
+        }
+
+        return(
+            <div>
+                <SearchBar/>
+                {renderVideoList()}        
+            </div>
+        )
+    }
+     
 }
 
 export default App;
