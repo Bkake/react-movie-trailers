7c277f32b14921b794614a1f80dda15d8cdc20c5
Bkake <bangaly.kake@gmail.com>
2018-06-02 16:16:26 +0200

    Add movie image and title in  VideoListItem

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const VideoListItem = (props) => {
-    return <li>Film demandé : {props.movie}</li>
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
+
+const VideoListItem = ({movie}) => {
+    return <li>
+                <img width="100px" height="100px" src={`${IMAGE_BASE_URL}${movie.poster_path}`} />
+                <h2>{movie.title}</h2>
+           </li>
 }
 
 export default VideoListItem;
